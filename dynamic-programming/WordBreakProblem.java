86326421b1bae02d57ded8a84ce4184e60d70071
Swastik Roy <swastik.roy@live.com>
2016-06-23 21:32:24 +0530

    Update WordBreakProblem.java
    Added changes to word break problem

--- dynamic-programming/WordBreakProblem.java
+++ dynamic-programming/WordBreakProblem.java
@@ -1,6 +1,6 @@
 class WordBreakProblem{
     
-    public static final Set<String> Dictionary = new HashSet<String>(){"my","name","car","is","john","i","have","a",",","."};
+    public static final Set<String> DICTIONARY = new HashSet<String>(){"my","name","car","is","john","i","have","a",",","."};
     
     public static void main(String[] args){
         
@@ -9,6 +9,27 @@ class WordBreakProblem{
     public static List<String> breakWords(String text){
         byte[][] checkWord = new byte[text.length][text.length];
         int[][] clipPosition = new int[text.length][text.length];
-        
+        int len = text.length;
+        for(int i=0;i<len;i++){
+            for(int j = 0,int k = i+j;k<len;j++){
+                String word = text.substring(j,k);
+                checkWord[j][k] = false;
+                if(DICTIONARY.contains(word)){
+                    checkWord[j][k] = true;
+                }else{
+                    for(int c = j+1;c<=k;c++){
+                        if(checkword[j][c] && checkword[c][k]){
+                            checkWord[j][k] = true;
+                            clipPosition[j][k] = c-j;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int N;
+        while(true){
+            
+        }
     }
 }
