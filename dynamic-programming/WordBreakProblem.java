62df3be5daa4e62327a56073481896bde06334da
Swastik Roy <swastik.roy@live.com>
2016-06-23 19:56:50 +0530

    Update WordBreakProblem.java
    Stareted writing code on word break problem

--- dynamic-programming/WordBreakProblem.java
+++ dynamic-programming/WordBreakProblem.java
@@ -1,2 +1,14 @@
 class WordBreakProblem{
+    
+    public static final Set<String> Dictionary = new HashSet<String>(){"my","name","car","is","john","i","have","a",",","."};
+    
+    public static void main(String[] args){
+        
+    }
+    
+    public static List<String> breakWords(String text){
+        byte[][] checkWord = new byte[text.length][text.length];
+        int[][] clipPosition = new int[text.length][text.length];
+        
+    }
 }
